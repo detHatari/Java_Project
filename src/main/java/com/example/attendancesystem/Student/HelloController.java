d7740cbc5b4d847df8ed78052f09f4b598b4b42b
detHatari <menrithydet@gmail.com>
2023-11-27 16:37:10 +0700

    Second Commit

--- src/main/java/com/example/attendancesystem/Student/HelloController.java
+++ src/main/java/com/example/attendancesystem/Student/HelloController.java
@@ -1,11 +1,8 @@
 package com.example.attendancesystem.Student;
 
-import com.example.attendancesystem.AlertHelper;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
